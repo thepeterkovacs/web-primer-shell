a71227a0391128975aa96fdf11b58bdc19c6264a
thepeterkovacs <peterkov37@gmail.com>
2023-08-30 22:52:00 +0200

    jsdoc to cn standard util

--- src/lib/utils/standard.ts
+++ src/lib/utils/standard.ts
@@ -1,6 +1,12 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs))
-}
+/**
+ * Combines multiple class values into a single class name string using tailwindcss utilities.
+ * @param {...ClassValue[]} inputs Class values to be combined.
+ * @returns {string} The combined class name string.
+ * @example
+ * const output = cn("text-red-500", { "bg-blue-200": false }, { "font-bold": true }, "py-2")
+ * //output = "text-red-500 font-bold py-2"
+ */
+export const cn = (...inputs: ClassValue[]): string => twMerge(clsx(inputs))
