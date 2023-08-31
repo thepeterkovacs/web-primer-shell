0bbd0e4da598f68427df15518ef027a03461521c
thepeterkovacs <peterkov37@gmail.com>
2023-08-31 14:11:41 +0200

    add roboto font to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,14 @@ import { ReactNode } from "react"
 
 import "@/styles/globals.css"
 
+import * as fonts from "@/lib/assets/fonts"
+
 import Providers from "@/components/providers/Providers"
 
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
 		<html>
-			<body>
+			<body className={fonts.roboto.className}>
 				<Providers>{children}</Providers>
 			</body>
 		</html>
