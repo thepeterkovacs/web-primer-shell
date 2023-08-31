7c0a8a6d126cd262f9630111c3a40835f8e77fcb
thepeterkovacs <peterkov37@gmail.com>
2023-08-31 19:25:45 +0200

    theme provider to providers

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -1,7 +1,12 @@
 import { PropsWithChildren } from "react"
 
 import QueryProvider from "./QueryProvider"
+import ThemeProvider from "./ThemeProvider"
 
 export default function Providers({ children }: PropsWithChildren): JSX.Element {
-	return <QueryProvider>{children}</QueryProvider>
+	return (
+		<QueryProvider>
+			<ThemeProvider>{children}</ThemeProvider>
+		</QueryProvider>
+	)
 }
