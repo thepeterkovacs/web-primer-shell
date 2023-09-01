2dadbdc8d68d90a80d41e05eba816fdc5b8a6fd3
thepeterkovacs <peterkov37@gmail.com>
2023-09-01 09:43:29 +0200

    title to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import { ReactNode } from "react"
 
 export default function Home(): ReactNode {
-	return <main></main>
+	return (
+		<main className='flex h-screen justify-center items-center'>
+			<h1 className='text-6xl'>Web Primer Shell</h1>
+		</main>
+	)
 }
