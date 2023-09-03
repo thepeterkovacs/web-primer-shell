876e50d8226142f46cb2583b0211c107fb6828f3
thepeterkovacs <peterkov37@gmail.com>
2023-09-03 19:12:20 +0200

    add api test helper and api test case type

---
+++ tests/helpers.ts
@@ -0,0 +1,43 @@
+import { expect, test } from "@playwright/test"
+
+export type ApiTestCase = {
+	title?: string
+	request: {
+		method: "delete" | "get" | "patch" | "post" | "put"
+		url: string
+		options?: {
+			data?: any
+			headers?: { [key: string]: string }
+			params?: { [key: string]: string | number | boolean }
+			timeout?: number
+		}
+	}
+	response: {
+		status: number
+		body?: {
+			[key: string]: any
+		}
+	}
+}
+
+export function API_TEST_HELPER(title: string, testCases: ApiTestCase[]) {
+	test.describe.parallel(title, () => {
+		testCases.map((testCase) => {
+			const { title, request, response } = testCase
+			const { method, url, options } = request
+			const { status, body } = response
+
+			test(title ?? `${method}__${url}`, async ({ request }) => {
+				const response = await request[method](url, options)
+
+				if (status) {
+					expect(response.status()).toBe(status)
+				}
+
+				if (body) {
+					expect(JSON.parse(await response.text())).toMatchObject(body)
+				}
+			})
+		})
+	})
+}
