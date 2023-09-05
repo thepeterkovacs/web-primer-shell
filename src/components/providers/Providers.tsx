01dba06fbbc049afc07472a4cc4616d02b1b0540
thepeterkovacs <peterkov37@gmail.com>
2023-09-05 23:04:46 +0200

    session provider to providers

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -1,16 +1,23 @@
+import { getServerSession } from "next-auth"
+
 import { PropsWithChildren } from "react"
 
 import DevTools from "../tools/DevTools"
 import QueryProvider from "./QueryProvider"
+import SessionProvider from "./SessionProvider"
 import ThemeProvider from "./ThemeProvider"
 
-export default function Providers({ children }: PropsWithChildren): JSX.Element {
+export default async function Providers({ children }: PropsWithChildren): Promise<JSX.Element> {
+	const session = await getServerSession()
+
 	return (
-		<QueryProvider>
-			<ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-				{children}
-				<DevTools />
-			</ThemeProvider>
-		</QueryProvider>
+		<SessionProvider session={session}>
+			<QueryProvider>
+				<ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+					{children}
+					<DevTools />
+				</ThemeProvider>
+			</QueryProvider>
+		</SessionProvider>
 	)
 }
