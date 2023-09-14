c8a08e14f1acd06fd03b97fd08423590c3ffe7ee
thepeterkovacs <peterkov37@gmail.com>
2023-09-14 16:51:10 +0200

    rename trpc client and server instances

--- src/app/_trpc/caller.ts
+++ src/app/_trpc/caller.ts
@@ -1,7 +1,7 @@
 import { appRouter } from "@/server/appRouter"
 import { httpBatchLink } from "@trpc/client"
 
-export const caller = appRouter.createCaller({
+export const trpcServer = appRouter.createCaller({
 	links: [
 		httpBatchLink({
 			url: "http://localhost:3000/api/trpc",
--- src/app/_trpc/client.ts
+++ src/app/_trpc/client.ts
@@ -1,4 +1,4 @@
 import { type AppRouter } from "@/server/appRouter"
 import { createTRPCReact } from "@trpc/react-query"
 
-export const trpc = createTRPCReact<AppRouter>({})
+export const trpcClient = createTRPCReact<AppRouter>({})
