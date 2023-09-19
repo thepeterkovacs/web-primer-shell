9070727216fa49155a46a0b841b9559e71e88e35
thepeterkovacs <peterkov37@gmail.com>
2023-09-19 14:49:55 +0200

    add shebang and remove main from package json

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { checkIfGitExists, cloneRepository } from "./commands/git.js"
 import { askIsReady, askProjectName } from "./commands/inquirer.js"
 import showTitle from "./commands/title.js"
