78bdbd7d43c6c5a4796ceefc1ba13290b720ff0a
thepeterkovacs <peterkov37@gmail.com>
2023-09-19 08:35:52 +0200

    add types to inquirer commands

--- cli/src/commands/inquirer.ts
+++ cli/src/commands/inquirer.ts
@@ -1,21 +1,25 @@
 import inquirer from "inquirer"
 
 export async function askIsReady() {
-	return await inquirer.prompt([
+	const answer: { ready: boolean } = await inquirer.prompt([
 		{
 			name: "ready",
 			type: "confirm",
 			message: "Ready to clone Web Primer Shell from GitHub?",
 		},
 	])
+
+	return answer.ready
 }
 
 export async function askProjectName() {
-	return await inquirer.prompt([
+	const answer: { name: string } = await inquirer.prompt([
 		{
 			name: "name",
 			type: "input",
 			message: "What is the name of your project?",
 		},
 	])
+
+	return answer.name
 }
