f8431301c20f43260e4e4b2bd3e30652e355e6b1
thepeterkovacs <peterkov37@gmail.com>
2023-09-19 08:25:39 +0200

    make show title async

--- cli/src/commands/title.ts
+++ cli/src/commands/title.ts
@@ -2,13 +2,19 @@ import chalk from "chalk"
 import figlet from "figlet"
 import gradient from "gradient-string"
 
-export default function showTitle() {
-	figlet.text("Web Primer Shell", { font: "Doom" }, function (err, data) {
-		if (err) {
-			console.log(chalk.bgRed("Error while initializing"))
-			console.log(chalk.red(err + "\n"))
-			process.exit(1)
-		}
-		console.log(gradient.retro(data))
+export default async function showTitle(): Promise<void> {
+	return new Promise<void>(function (resolve) {
+		figlet.text("Web Primer Shell", { font: "Doom" }, function (err, data) {
+			if (err) {
+				console.log(chalk.bgRed("Error while initializing"))
+				console.log(chalk.red(err + "\n"))
+
+				process.exit(1)
+			}
+
+			console.log(gradient.retro(data))
+
+			return resolve()
+		})
 	})
 }
