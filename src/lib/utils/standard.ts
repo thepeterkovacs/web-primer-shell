f0b9a85e971d909426857c7558a0c2cf234ac788
thepeterkovacs <peterkov37@gmail.com>
2023-10-03 11:24:31 +0200

    extract substring to standard utils

--- src/lib/utils/standard.ts
+++ src/lib/utils/standard.ts
@@ -22,3 +22,22 @@ export const getHtmlFromUrl = async (url: string): Promise<string> => {
 	const res = await fetch(url, { cache: "no-store" })
 	return await res.text()
 }
+
+/**
+ * Extracts a substring from a string, given a prefix and suffix.
+ * @param {string} str The string to extract the substring from.
+ * @param {string} prefix Prefix of the substring to extract.
+ * @param {string} suffix Suffix of the substring to extract.
+ * @returns {string} The substring extracted from the string, or an empty string if the substring could not be found.
+ * @example
+ * const output = extractSubstring("Some string as str.", "Some ", " as str.")
+ * //output = "string"
+ */
+export const extractSubstring = (str: string, prefix: string, suffix: string): string => {
+	const startIndex = str.indexOf(prefix)
+	const endIndex = str.indexOf(suffix)
+
+	const subString = str.slice(startIndex + prefix.length, endIndex)
+
+	return subString
+}
