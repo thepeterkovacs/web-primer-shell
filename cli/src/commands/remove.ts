6b6d7498f20acd87d45e3c83a8c9a90718eac189
thepeterkovacs <peterkov37@gmail.com>
2023-10-05 23:27:47 +0200

    add remove unnecessary directories command

--- cli/src/commands/remove.ts
+++ cli/src/commands/remove.ts
@@ -4,6 +4,19 @@ import { oraPromise } from "ora"
 
 import error from "../utils/error.js"
 
+export async function removeUnnecessaryDirectories(name: string): Promise<void> {
+	try {
+		await oraPromise(execa("rd", ["/s", "/q", `${name}\\cli`], { shell: true }), {
+			text: "Removing unnecessary directories...",
+			successText: "Unnecessary directories removed successfully",
+			spinner: cliSpinners.binary,
+			color: "yellow",
+		})
+	} catch (err) {
+		error("Error while removing unnecessary directories", err)
+	}
+}
+
 export async function removeUnnecessaryFiles(name: string): Promise<void> {
 	try {
 		await oraPromise(
@@ -15,9 +28,7 @@ export async function removeUnnecessaryFiles(name: string): Promise<void> {
 					`${name}\\LICENSE.md`,
 					`${name}\\SECURITY.md`,
 					`${name}\\README.md`,
-					`${name}\\cli\\CHANGELOG.md`,
-					`${name}\\cli\\LICENSE.md`,
-					`${name}\\cli\\README.md`,
+					`${name}\\cli`,
 				],
 				{
 					shell: true,
