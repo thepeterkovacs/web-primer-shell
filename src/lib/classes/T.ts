b18d92ecb8f3af5f3ca464f4d897fe10e97b9e62
thepeterkovacs <peterkov37@gmail.com>
2023-10-13 11:50:27 +0200

    add jsdoc and changes to the time class

--- src/lib/classes/T.ts
+++ src/lib/classes/T.ts
@@ -1,3 +1,6 @@
+/**
+ * A class for representing and calculating time in days, hours, minutes, seconds, and milliseconds.
+ */
 export default class T {
 	private days: number
 	private hours: number
@@ -5,34 +8,74 @@ export default class T {
 	private seconds: number
 	private milliseconds: number
 
-	constructor() {
-		this.days = 0
-		this.hours = 0
-		this.minutes = 0
-		this.seconds = 0
-		this.milliseconds = 0
+	constructor(
+		days?: number,
+		hours?: number,
+		minutes?: number,
+		seconds?: number,
+		milliseconds?: number
+	) {
+		this.days = days ?? 0
+		this.hours = hours ?? 0
+		this.minutes = minutes ?? 0
+		this.seconds = seconds ?? 0
+		this.milliseconds = milliseconds ?? 0
 	}
 
+	/**
+	 * Sets the number of days.
+	 * @param {number} days Number of days.
+	 * @returns The current `T` object.
+	 */
+	d(days: number): T {
+		this.days = days
+		return this
+	}
+
+	/**
+	 * Sets the number of hours.
+	 * @param {number} hours Number of hours.
+	 * @returns The current `T` object.
+	 */
 	h(hours: number): T {
 		this.hours = hours
 		return this
 	}
 
+	/**
+	 * Sets the number of minutes.
+	 * @param {number} minutes Number of minutes.
+	 * @returns The current `T` object.
+	 */
 	m(minutes: number): T {
 		this.minutes = minutes
 		return this
 	}
 
+	/**
+	 * Sets the number of seconds.
+	 * @param {number} seconds Number of seconds.
+	 * @returns The current `T` object.
+	 */
 	s(seconds: number): T {
 		this.seconds = seconds
 		return this
 	}
 
+	/**
+	 * Sets the number of milliseconds.
+	 * @param {number} milliseconds Number of milliseconds.
+	 * @returns The current `T` object.
+	 */
 	ms(milliseconds: number): T {
 		this.milliseconds = milliseconds
 		return this
 	}
 
+	/**
+	 * Calculates the number of days from the `T` object.
+	 * @returns The number of days.
+	 */
 	toD(): number {
 		return (
 			this.days +
@@ -43,6 +86,10 @@ export default class T {
 		)
 	}
 
+	/**
+	 * Calculates the number of hours from the `T` object.
+	 * @returns The number of hours.
+	 */
 	toH(): number {
 		return (
 			this.days * 24 +
@@ -53,6 +100,10 @@ export default class T {
 		)
 	}
 
+	/**
+	 * Calculates the number of minutes from the `T` object.
+	 * @returns The number of minutes.
+	 */
 	toM(): number {
 		return (
 			this.days * 24 * 60 +
@@ -63,6 +114,10 @@ export default class T {
 		)
 	}
 
+	/**
+	 * Calculates the number of seconds from the `T` object.
+	 * @returns The number of seconds.
+	 */
 	toS(): number {
 		return (
 			this.days * 24 * 60 * 60 +
@@ -73,6 +128,10 @@ export default class T {
 		)
 	}
 
+	/**
+	 * Calculates the number of milliseconds from the `T` object.
+	 * @returns The number of milliseconds.
+	 */
 	toMs(): number {
 		return (
 			this.days * 24 * 60 * 60 * 1000 +
