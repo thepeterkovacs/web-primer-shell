ea5cfe0dfc4af5a3f8b424577432a57217bf7717
thepeterkovacs <peterkov37@gmail.com>
2023-10-22 23:13:12 +0200

    format with prettier-plugin-tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
 	return (
-		<main className="flex h-screen justify-center items-center">
+		<main className="flex h-screen items-center justify-center">
 			<h1 className="text-6xl">Web Primer Shell</h1>
 		</main>
 	)
--- src/components/tools/ThemeToggle.tsx
+++ src/components/tools/ThemeToggle.tsx
@@ -12,8 +12,8 @@ export default function ThemeToggle(): JSX.Element {
 
 	return (
 		<Button variant="ghost" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-			<SunIcon className="w-6 h-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-			<MoonIcon className="w-6 h-6 absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+			<SunIcon className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+			<MoonIcon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
 			<span className="sr-only">Toggle theme</span>
 		</Button>
 	)
