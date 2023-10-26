aacb71c50345b229263acda415b7acafdf853cc5
thepeterkovacs <peterkov37@gmail.com>
2023-10-26 17:00:46 +0200

    intl client provider

---
+++ src/components/providers/IntlClientProvider.tsx
@@ -0,0 +1,17 @@
+import { NextIntlClientProvider } from "next-intl"
+
+import { PropsWithChildren } from "react"
+
+interface Props extends PropsWithChildren {
+	locale: string
+}
+
+export default async function IntlClientProvider({ children, locale }: Props) {
+	const messages = (await import(`../../lib/i18n/messages/${locale}.json`)).default
+
+	return (
+		<NextIntlClientProvider locale={locale} messages={messages}>
+			{children}
+		</NextIntlClientProvider>
+	)
+}
