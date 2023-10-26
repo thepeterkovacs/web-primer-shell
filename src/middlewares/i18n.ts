76b6acd928956e1b27ce432b7095e8b581275e25
thepeterkovacs <peterkov37@gmail.com>
2023-10-26 17:01:56 +0200

    i18n config to middleware

--- src/middlewares/i18n.ts
+++ src/middlewares/i18n.ts
@@ -2,9 +2,12 @@ import createMiddleware from "next-intl/middleware"
 
 import { NextRequest, NextResponse } from "next/server"
 
+import { defaultLocale, locales } from "@/lib/i18n/config"
+
 export default function i18n(): (request: NextRequest) => NextResponse<unknown> {
 	return createMiddleware({
-		locales: [],
-		defaultLocale: "",
+		locales,
+		defaultLocale,
+		localePrefix: "never",
 	})
 }
